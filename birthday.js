557ab5abf80e9cc5390e65a0f537e5c1d882235b
Volz <justin.volz@nydm2016489.addomain.org>
2017-06-15 17:11:55 -0400

    good

--- birthday.js
+++ birthday.js
@@ -31,7 +31,7 @@ daysPlural = ((days)=>{
 });
 
 //makes the birthday calculated paragraph with flavor text!
-    birthdayParagraph.innerHTML = "<p>Hello there!<br>---<br>It's your birthday (kind of) soon. How soon you ask?<br>Well actually, " + daysTillBirthday + " day"+ daysPlural(11) + " away! I really shouldn’t<br>probably know that, but I’m a computer.<br> (plus you told me, so it’s cool.)</p><p>~</p><p>Yours truely,<br> Computer #4367A47<p>";
+    birthdayParagraph.innerHTML = "<p>Hello there!<br>---<br>It's your birthday (kind of) soon. How soon you ask?<br>Well actually, " + daysTillBirthday + " day"+ daysPlural(11) + " away! I really shouldn\’t<br>probably know that, but I’m a computer.<br> (plus you told me, so it’s cool.)</p><p>~</p><p>Yours truely,<br> Computer #4367A47<p>";
 
 
 
